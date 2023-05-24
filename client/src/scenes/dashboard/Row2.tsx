0a72ba8fbd308e20264d1b37d07ac7ca475c6dbf
mfbernasol <mfbernasol@gmail.com>
2023-05-23 20:30:14 -0500

    setting up layouts

---
+++ client/src/scenes/dashboard/Row2.tsx
@@ -0,0 +1,14 @@
+import DashboardBox from '@/components/DashboardBox';
+import React from 'react';
+
+type Props = {};
+
+const Row2 = (props: Props) => {
+  return (
+    <>
+      
+    </>
+  );
+};
+
+export default Row2;
--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,4 +1,7 @@
-import { Box, useMediaQuery, useTheme } from '@mui/material';
+import { Box, useMediaQuery,} from '@mui/material';
+import Row1 from './Row1';
+import Row2 from './Row2';
+import Row3 from './Row3';
 
 type Props = {};
 
@@ -48,7 +51,7 @@ const gridTemplateSmallScreens = `
     "j"
 `;
 
-const Dashboard = (props: Props) => {
+const Dashboard = () => {
   const isAboveMediumScreens = useMediaQuery('(min-width: 1200px)');
   return (
     <Box
@@ -70,18 +73,12 @@ const Dashboard = (props: Props) => {
             }
       }
     >
-      <Box bgcolor='#fff' gridArea='a'></Box>
-      <Box bgcolor='#fff' gridArea='b'></Box>
-      <Box bgcolor='#fff' gridArea='c'></Box>
-      <Box bgcolor='#fff' gridArea='d'></Box>
-      <Box bgcolor='#fff' gridArea='e'></Box>
-      <Box bgcolor='#fff' gridArea='f'></Box>
-      <Box bgcolor='#fff' gridArea='g'></Box>
-      <Box bgcolor='#fff' gridArea='h'></Box>
-      <Box bgcolor='#fff' gridArea='i'></Box>
-      <Box bgcolor='#fff' gridArea='j'></Box>
+    
+      <Row1/>
+      <Row2/>
+      <Row3/>
     </Box>
   );
-};
+}; 
 
 export default Dashboard;
